fbb35227d3598b529e8be93cc5094eac05ca92d6
Abraham Cabrera <testingdfall@gmail.com>
2025-10-16 13:06:02 +0200

    Add server configuration to Vite for frontend with host, port, and allowed hosts

--- frontend_main/vite.config.js
+++ frontend_main/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+   server: {
+    host: '127.0.0.1', // escucha en 127.0.0.1 en vez de localhost
+    port: 3000,        // puerto del frontend
+     strictPort: true,  // falla si el puerto está ocupado
+      allowedHosts: [
+      '127.0.0.1.nip.io',    // tu host principal
+      '.127.0.0.1.nip.io'    // para subdominios dinámicos como cliente1, cliente2
+    ],
+  },
+});
